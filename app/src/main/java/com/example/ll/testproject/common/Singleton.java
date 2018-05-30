b12827533c08d1d6eecf8882f0bfb88839ba8628
ll <llu1987@aliyun.com>
2018-05-30 14:56:39 +0800

    frist commit

---
+++ app/src/main/java/com/example/ll/testproject/common/Singleton.java
@@ -0,0 +1,31 @@
+package com.example.ll.testproject.common;
+
+import java.security.Signature;
+
+/**
+ * Created by ll on 2018/5/30.
+ */
+
+public class Singleton {
+    private Singleton() {
+
+    }
+    private static volatile Singleton instance = null;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (null == instance) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private static class LazyHolder{
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static final
+}
